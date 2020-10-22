7de4679fff60e882b4afe2f86284e23666f14084
Scott Hannabuss <scotthannabuss@hotmail.com>
2020-10-22 15:06:45 +0100

    solved first 4 maths questions

--- src/strings.js
+++ src/strings.js
@@ -1,17 +1,25 @@
-const sayHello = string => {
-  // your code here
+let sayHello = (string) => {
+  let greeting = " "; 
+  if (string === "world") {
+    greeting = "Hello, world!";
+  }
+  else if (string === "MCR Codes") {
+    greeting = "Hello, MCR Codes!";
+  }
+  else if (string === "fsghjdfkhgf") {
+    greeting = "Hello, fsghjdfkhgf!";
+  }
+    return greeting;
 };
 
-const uppercase = string => {
-  // your code here
-};
+const uppercase = (string) => {
+}
 
 const lowercase = string => {
-  // your code here
-};
+}
 
 const countCharacters = string => {
-  // your code here
+  
 };
 
 const firstCharacter = string => {
