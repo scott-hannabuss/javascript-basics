74a62d86f683c7c72c45e29133128b33bffa8893
Scott Hannabuss <scotthannabuss@hotmail.com>
2020-10-24 13:02:00 +0100

    completed boolean challenges

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,106 @@
 const negate = a => {
-  // your code here
+  return !a;
 };
 
 const both = (a, b) => {
-  // your code here
+  return a && b;
 };
 
 const either = (a, b) => {
-  // your code here
+  return a || b;
 };
 
 const none = (a, b) => {
-  // your code here
+  if (a === false && b === false) {
+    return true;
+  }
+
+  return false;
 };
 
 const one = (a, b) => {
-  // your code here
+  if (a === true && b === false) {
+    return true;
+  }
+  if (a === false && b === true) {
+    return true;
+  }
+  return false;
 };
 
 const truthiness = a => {
-  // your code here
+  if (a) {
+    return true;
+  }
+
+  return false;
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  if (a === b) {
+    return true;
+  }
+
+  return false;
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  if (a > b) {
+    return true;
+  }
+
+  return false;
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  if (a <= b) {
+    return true;
+  }
+
+  return false;
 };
 
 const isOdd = a => {
-  // your code here
+  if (a % 2 === 0) {
+    return false;
+  }
+
+  return true;
 };
 
 const isEven = a => {
-  // your code here
+  if (a % 2 === 0) {
+    return true;
+  }
+
+  return false;
 };
 
 const isSquare = a => {
-  // your code here
+  if (Math.sqrt(a) % 1 === 0) {
+    return true;
+  }
+
+  return false;
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  if (char === string.charAt(0)) {
+    return true;
+  }
+
+  return false;
 };
 
 const containsVowels = string => {
-  // your code here
-};
+  const str = string; 
+  const result = /[aeiou]/gi.test(str) 
+    return result
+}
+
 
 const isLowerCase = string => {
-  // your code here
+  return string === string.toLowerCase(); 
 };
 
 module.exports = {
