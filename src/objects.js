544e3cc183ba0ee9c7b8093cf234caab342d20d1
Scott Hannabuss <scotthannabuss@hotmail.com>
2020-10-28 21:35:39 +0000

    completed object challenges

--- src/objects.js
+++ src/objects.js
@@ -1,42 +1,64 @@
 const createPerson = (name, age) => {
-  // your code here
-};
+
+  var obj = new Object();
+  obj.name = name;
+  obj.age = age;
+  return obj;
+}
 
 const getName = object => {
-  // your code here
+  answer = object.name;
+  return answer;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return (object[property]);
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return property in object;
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 const getAges = people => {
-  // your code here
+  var ages = people.map(a => a.age);
+  return ages;
 };
 
 const findByName = (name, people) => {
-  // your code here
+  let answer = people.find(a => a.name === name);
+  return answer;
 };
 
 const findHondas = cars => {
-  // your code here
+  let answer = cars.filter(a => a.manufacturer === 'Honda');
+  return answer;
 };
 
 const averageAge = people => {
-  // your code here
+  let count = people.reduce((a, b) => a + b.age, 0);
+  var answer = (count / people.length);
+  return answer;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
-};
+  let bill = {
+    name: "Bill",
+    age: 40,
+  }
+  bill.introduce = function (name, age) {
+    return "Hi Fred, my name is" + " " + this.name + " " + "and I am" + " " + this.age + "!";
+  }
+  return bill;
+}
 
 module.exports = {
   createPerson,
